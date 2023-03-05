ae146958ecc044de32c04534079331f5cef151cd
Kingsley Baah Brew <prince@Kingsleys-MacBook-Pro.local>
2023-03-05 16:44:59 +0000

    nyc-assignment api commit

--- annotations/Env.ts
+++ annotations/Env.ts
@@ -3,7 +3,7 @@ import { EnvironmentVariables } from "../utils";
 /**
  * @author Kingsley Baah Brew <kingsleybrew@gmail.com>
  * @params key | string
- * @todo This decorator accepts an object key to read data from the app.properties.json
+ * @todo This decorator accepts an object key to read data from the .env
  */
 export default function Env(key: string) {
     return function(target: any, propertyKey: any) {
